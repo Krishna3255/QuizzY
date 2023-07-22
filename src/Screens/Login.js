af3a35483ee13d4c48f0d7a142ddac64e9aad9cd
Krishna <krishnapeteti11@gmail.com>
2023-07-23 03:41:32 +0530

    Signup Intinal commit

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -10,12 +10,12 @@ export default function Login() {
       <form>
         <h2 className='heading'>Log In</h2>
           <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Email address</label>
+            <label htmlForfor="exampleInputEmail1" className="form-label">Email address</label>
             <input type="email" className="form-control w-75" id="exampleInputEmail1" aria-describedby="emailHelp"/>
               
           </div>
           <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Password</label>
+            <label htmlForfor="exampleInputPassword1" className="form-label">Password</label>
             <input type="password" className="form-control w-75" id="exampleInputPassword1"/>
           </div>
           <button type="submit" className="btn btn-success w-25">Login</button>
--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,39 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Components/Navbar'
 import { Link } from 'react-router-dom'
 
+
+
+
 export default function SignUp() {
+
+  // Dynamically collecting the data from the user 
+  const [credintals, setcredintals] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profession: ""
+  });
+
+
+  const handleChange = (event) => {
+
+    setcredintals({ ...credintals, [event.target.name]: event.target.value });
+
+  };
+
+  // On submit the user details the stored in the database under user collection
+
+  const handleSubmit= async (e)=>{
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/creatuser",{
+        method:'POST',
+        headers:{
+          "Content-Type":"application/json",
+        },
+        body:JSON.stringify({
+          name:credintals.name,
+          email:credintals.email,
+          password:credintals.password,
+          profession:credintals.profession
+        }),
+    });
+
+    const json = await response.json();
+    console.log(json);
+
+    if(!json.success){
+      alert("nope an error occured !!");
+    }
+    else{
+      alert("Successfully registered .... Log In")
+    }
+
+     
+
+
+
+  };
+
+
   return (
     <div>
       <div><Navbar /></div>
       <div className='container mt-5 w-50 border border-dark'>
-        <form>
-        <h2 className='heading'>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <h2 className='heading'>Sign Up</h2>
           <div className="mb-3">
-            <label for="exampleInputName" className="form-label"> Full Name</label>
-            <input type="email" className="form-control w-75" id="exampleInputName" />
-
+            <label htmlFor="exampleInputName" className="form-label"> Full Name</label>
+            <input type="text" className="form-control w-75" id="exampleInputName" name='name'
+              value={credintals.name}
+              onChange={handleChange} />
           </div>
 
           <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control w-75" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control w-75" id="exampleInputEmail1" aria-describedby="emailHelp" value={credintals.email} onChange={handleChange} name='email' />
 
           </div>
           <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control w-75" id="exampleInputPassword1" />
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control w-75" id="exampleInputPassword1" name='password' value={credintals.password} onChange={handleChange}/>
           </div>
           <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label ">Profession</label>
-          <select class="form-select w-75" aria-label="Default select example">
-            <option selected disabled>select the option</option>
-            <option value="1">Student</option>
-            <option value="2">Teacher</option>
-            <option value="3">others</option>
-          </select>
+            <label htmlFor="exampleInputPassword1" className="form-label ">Profession</label>
+            <select class="form-select w-75" aria-label="Default select example">
+              <option selected disabled>select the option</option>
+              <option name='Student' value={credintals.Student} onChange={handleChange}  >Student</option>
+              <option name='Teacher' value={credintals.profession} >Faculty</option>
+              <option name='Others' value={credintals.profession} onChange={handleChange}>Others</option>
+            </select>
           </div>
-          <div><Link to="/login"><button type="submit" className="btn btn-dark mt-3 w-25 btn-signup">SignUp</button></Link></div>
+          <button type="submit" className="btn btn-dark mt-3 w-25 btn-signup">SignUp</button>
           <div><Link to="/Login" className='alreadyuser'>Already an user</Link></div>
 
         </form>
